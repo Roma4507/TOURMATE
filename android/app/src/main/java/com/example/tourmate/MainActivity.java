2e7ad9742bc5829497d40e955a006585ca41f03c
Roma <romakataria15@gmail.com>
2022-12-06 12:40:09 +0530

    Homepage

---
+++ android/app/src/main/java/com/example/tourmate/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.tourmate;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:tourmate/main.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Home()
+));
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome tourist!"),
+        backgroundColor: Colors.blueGrey[800],
+      ),
+            body: InkWell(
+              onTap: (){
+                print("Let's begin your journey");
+              },
+              child: Container(
+                //width: MediaQuery.of(context).size.width,
+
+                height: double.infinity,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  //color: Colors.blueGrey,
+                  image: DecorationImage(image: AssetImage("assets/Tour.jpeg"),fit: BoxFit.fill)
+                ),
+
+        ),
+            ),
+
+    );
+  }
+}
