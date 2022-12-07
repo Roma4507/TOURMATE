0ef631a1a815f8c13bfa94be36a81be55e67611b
Roma <romakataria15@gmail.com>
2022-12-07 12:11:47 +0530

    Homepage

---
+++ lib/Pages/Home_Screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+
+class Homepage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome tourist!"),
+        backgroundColor: Colors.blueGrey[800],
+      ),
+      body: InkWell(
+        onTap: (){
+          print("Let's begin your journey");
+        },
+        child: Container(
+          //width: MediaQuery.of(context).size.width,
+
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            //color: Colors.blueGrey,
+              image: DecorationImage(image: AssetImage("assets/tour_1.jpeg"),fit: BoxFit.fill)
+          ),
+
+        ),
+      ),
+
+    );
+  }
+}
